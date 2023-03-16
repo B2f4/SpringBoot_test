c81491b4765fe741aa2841330b7273fef7f44fed
B2f4 <95875422+B2f4@users.noreply.github.com>
2023-03-16 16:58:22 +0100

    Implemented login

---
+++ demo/demo/src/main/java/com/example/demo/models/Utente.java
@@ -0,0 +1,53 @@
+package com.example.demo.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Utente{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int id;
+	String username;
+	String password;
+	@ManyToOne
+	Role role;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+
+
+	
+	
+	
+	
+}
---
+++ demo/demo/src/main/java/com/example/demo/services/IUtenteService.java
@@ -0,0 +1,17 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import com.example.demo.dto.LoginDTO;
+import com.example.demo.dto.UtenteDTO;
+
+public interface IUtenteService {
+
+	public UtenteDTO createUser (UtenteDTO utenteDTO);
+	public UtenteDTO deleteUser (int id);
+	public List<UtenteDTO> getAll();
+	public UtenteDTO getById (int id);
+	public UtenteDTO updateUser (int id, UtenteDTO utenteDTO);
+	public UtenteDTO loginUser (LoginDTO loginDTO);
+	
+}
